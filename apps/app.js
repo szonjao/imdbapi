95b15c79acf4271bd9cbe2585202232307b8e96c
szonjao <szonja.o@me.com>
2016-01-01 23:39:51 +0100

    ainitial

---
+++ apps/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	$("#query").click(function(){
+		/*alert("test");*/
+		var searchTerm = $("#search-term").val();
+		submitValuation(searchTerm);
+	});
+	$("#search-term").on("keydown", function(e) {
+		if (e.which === 13) {
+			//alert("testEnter");
+			var searchTerm = $("#search-term").val();
+			submitValuation(searchTerm);
+		}
+	});
+	$("#search-term").focus(
+    function(){
+        $(this).val('');
+    });
+});
+
+function submitValuation(searchTerm){ 
+
+	var params = {
+		part: 'snippet',
+		key: 'apikey',
+		q: searchTerm
+	}
+
+	var url = 'http://www.omdbapi.com/?s=' + searchTerm + '&r=json'
+	$.getJSON(url, function(data){
+		displayResults(data.Search);
+	});
+}
+
+
+function displayResults(results){
+	console.log(results);
+	$.each(results, function(index,value){
+		$("#search-results").html(" ");
+		$("#search-term").blur();
+		$("#search-results").append('<img src="' + value.Poster + '"></br>');
+		$("#search-results").append(value.Title + ' ' + value.Year + '</br>' );
+	});
+}
